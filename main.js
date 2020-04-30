6b07860086369942acf2862ee470630663526e31
Your Name <you@example.com>
2020-04-30 19:43:42 +0530

    working audio recorder and player

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
+import MicRecorder from 'mic-recorder-to-mp3';
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const Mp3Recorder = new MicRecorder({ bitRate: 128 });
+
+
 
 module.exports = {
    entry: './main.js',
